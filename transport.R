f6c69bcabba9fae106729432f38fe6b2f1e33070
notesofdabbler <shanki@Shankars-MacBook-Pro.local>
2020-06-30 13:08:33 -0400

    modified rosen and transport models to use import pyomo with convert false option

--- transport.R
+++ transport.R
@@ -1,9 +1,10 @@
 library(reticulate)
 library(dplyr)
 
-source("operators.R")
+bi = import_builtins()
 
-pyo = import("pyomo.environ")
+pyo = import("pyomo.environ", convert = FALSE)
+source("operators.R")
 
 plants = c("seattle", "san-diego")
 mkts = c("new-york", "chicago", "topeka")
@@ -33,41 +34,53 @@ dict_from_df = function(df) {
 cost = dict_from_df(costdf %>% select(plants, mkts, cost))
 dict_from_df(costdf)
 
-model = pyo$ConcreteModel()
-model$x = pyo$Var(plants, mkts, bounds = tuple(0, NULL))
+M = pyo$ConcreteModel()
+M$plants = pyo$Set(initialize = c("seattle", "san-diego"))
+M$mkts = pyo$Set(initialize = c("new-york", "chicago", "topeka"))
+
+M$cap = pyo$Param(M$plants, initialize = list("seattle" = 350, "san-diego" = 600))
+M$dem = pyo$Param(M$mkts, initialize = list("new-york" = 325, "chicago" = 300, "topeka" = 275))
+M$cost = pyo$Param(M$plants, M$mkts, initialize = cost)
+
+M$x = pyo$Var(M$plants, M$mkts, bounds = tuple(0, NULL))
 
 supply_rule = function(m, i) {
   supply = 0
-  for (j in mkts) {
+  for (j in bi$list(m$mkts)) {
+#    print(j)
     supply = supply + m$x[tuple(i, j)]
   }
-  expr = supply <= cap[[i]]
+  expr = supply <= m$cap[[i]]
   return(expr)
 }
-model$supply_cons = pyo$Constraint(plants, rule = supply_rule)
+M$supply_cons = pyo$Constraint(M$plants, rule = supply_rule)
 
 demand_rule = function(m, j) {
   demand = 0
-  for (i in plants) {
+  for (i in bi$list(m$plants)) {
     demand = demand + m$x[tuple(i, j)]
   }
-  expr = demand >= dem[[j]]
+  expr = demand >= m$dem[[j]]
   return(expr)
 }
-model$demand_cons = pyo$Constraint(mkts, rule = demand_rule)  
+M$demand_cons = pyo$Constraint(M$mkts, rule = demand_rule)  
 
 objective_rule = function(m) {
   totcost = 0
-  for (i in plants) {
-    for (j in mkts) {
-      totcost = totcost + cost[tuple(i, j)] * model$x[tuple(i, j)]
+  for (i in bi$list(m$plants)) {
+    for (j in bi$list(m$mkts)) {
+      totcost = totcost + m$cost[tuple(i, j)] * m$x[tuple(i, j)]
     }
   }
   return(totcost)
 }
-model$objective = pyo$Objective(rule = objective_rule, sense = pyo$minimize)
+M$objective = pyo$Objective(rule = objective_rule, sense = pyo$minimize)
 
 opt = pyo$SolverFactory('glpk')
-results = opt$solve(model)
+results = opt$solve(M)
 
-model$x$display()
+M$x$display()
+
+for(j in bi$list(M$mkts)) {
+  print(j)
+}
